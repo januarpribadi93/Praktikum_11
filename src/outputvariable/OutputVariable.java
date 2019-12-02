b079cae465e5a6f742faa64e11388591144ecc9c
ahmadalfaruq <ahmadalfaruq33@gmail.com>
2019-12-02 10:14:58 -0800

    SEMANGAT

---
+++ src/outputvariable/OutputVariable.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package outputvariable;
+
+/**
+ *
+ * @author SMK PGRI 2 SUMEDANG
+ */
+public class OutputVariable
+ {
+ public static void main( String[] args ){
+ int value = 10;
+ char x;
+ x = 'A';
+
+ System.out.println( value );
+ System.out.println("The value of x=" + x );
+ }
+    
+}
---
+++ src/outputvariable/TestAND.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package outputvariable;
+
+/**
+ *
+ * @author SMK PGRI 2 SUMEDANG
+ */
+ public class TestAND
+ {
+ public static void main( String[] args ){
+
+ int i = 0;
+ int j = 10;
+ boolean test= false;
+
+ //demonstrasi &&
+ test = (i > 10) && (j++ > 9);
+ System.out.println(i);
+ System.out.println(j);
+ System.out.println(test);
+
+ //demonstrasi &
+ test = (i > 10) & (j++ > 9);
+ System.out.println(i);
+ System.out.println(j);
+ System.out.println(test);
+ }
+ } 
